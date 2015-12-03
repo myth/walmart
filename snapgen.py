8587bb503654365667b54b821007f8454aef7015
myth <myth@overflow.no>
2015-12-03 18:39:10 +0100

    Add some virkar questions

--- snapgen.py
+++ snapgen.py
@@ -3,19 +3,14 @@ from copy import deepcopy
 from itertools import product
 
 # Change these variables to suit your needs
-SUBJECT = 'Informasjonssystemer'
-TERM_PREFIX = 'What best describes'
+SUBJECT = 'Virksomhetsarkitektur og innovasjon'
+TERM_PREFIX = 'In the context of Enterprise Modeling, what best illustrates'
 TERM_SUFFIX = '?'
 DEFINITIONS = [
-    ('Organizational culture', 'The major understanding and assumptions for a business'),
-    ('Organizational change', 'How organizations plan for, implement, and handle change'),
-    ('Change model', 'Representation of change theories that identifies the phases of change'),
-    ('Unfreezing', 'Ceasing old habits and creating a climate receptive to change'),
-    ('Moring', 'The process of learning new methods and systems'),
-    ('Refreezing', 'Reinforce changes to make the new process accepted'),
-    ('Organizational learning', 'Adapting to new conditions or altering practices over time'),
-    ('Return of Investment (ROI)', 'Profits generated as a percentage of the investment in IS'),
-    ('Earning growth', 'The increase in profit'),
+    ('Perspective', 'What are the important components?'),
+    ('Framework', 'How do the different problems and questions relate to each other?'),
+    ('Procedure, Notation and Concepts', 'What questions have to be asked?'),
+    ('Ways of cooperation', 'Who asks, and who replies?'),
 ]
 
 # Do not change anything below...
